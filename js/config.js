eebbe76ea5db8c7b4b25200631847927b853f39a
licarpen <carpenter.lisa@gmail.com>
2020-05-20 23:28:24 +0000

    change app client

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_IRpo4r1RG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '33hg512vqr30itirdmiks57bpo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2eger570g2chm3lslfo0961j8g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
